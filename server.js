47dd97cbc998c080ecbe6f91e2097d50c9cec5f7
Danmaza <danmaza2004@gmail.com>
2025-08-06 22:48:43 -0500

    done

--- server.js
+++ server.js
@@ -1,75 +1,59 @@
-//load express modules
+// Load modules
 const express = require('express');
 const axios = require('axios');
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 
-
-//create app with express
+// Create app
 const app = express();
 
-//set views property
+// Set view engine
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded());
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
- app.get("/", function(request, response){
-     response.render("index", {
-         data: "Welcome"
-     })
- });
+// API URL
+const API_URL = "https://v6.exchangerate-api.com/v6/cb1d0861b7f003a6bec09003/latest/USD";
 
-// using some local memory information 
-
-app.get('/mascots', function(req, res) {
-    var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tuxedo', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-
-    res.render('mascots', {
-        mascots: mascots,
-        tagline: tagline
-    });
+// Root route – first page load
+app.get("/", function (req, res) {
+    res.render("index", { currencies: null });
 });
 
+// Load route – fetch random currency data
+app.get("/load", function (req, res) {
+    axios.get(API_URL)
+        .then((response) => {
+            const rates = response.data.conversion_rates;
+            const keys = Object.keys(rates).filter(code => code !== "USD");
 
-// Code to call Third party server API and processing retrived JSON data (from API) here in server.js file
+            // Pick 3 unique random currencies
+            const selected = [];
+            while (selected.length < 3) {
+                const rand = keys[Math.floor(Math.random() * keys.length)];
+                if (!selected.includes(rand)) selected.push(rand);
+            }
 
-app.get('/superhero', function(req, res){
-    
-    // This is the url to call superheros api from superheroapi.com server to get super heros information i.e., hulk, spider etc.,
-    var url = "https://www.superheroapi.com/api.php/10221405381743383/search/hulk";
-    axios.get(url)
-        .then((response)=>{
-            // This is how you get required data out of whole api call data list
-            let myHeroArray = response.data.results;
-            let hero = myHeroArray[0];
-            let aliases = hero.biography.aliases;
-            
-            res.render('superhero', {
-                aliases: aliases
+            // Prepare display data
+            const currencies = selected.map(code => {
+                const rate = rates[code];
+                const costInUSD = (100 / rate).toFixed(2);
+                return {
+                    code,
+                    rate,
+                    costInUSD
+                };
             });
+
+            res.render("index", { currencies: currencies });
+        })
+        .catch((error) => {
+            console.error("API error:", error);
+            res.send("Failed to load exchange rates.");
         });
-  
-  })
-
-// Code to call Third party server API and processing retrived JSON data (from API) in ejs file
-app.get('/appleitunes', function(req, res) {
-
-    //itunes API call
-    axios.get('https://itunes.apple.com/search?term=radiohead')
-    .then((response)=>{
-        let musicData = response.data;
-        console.log(musicData);
-        res.render('appleitunes', {
-            music: musicData
-        });
-    });
-
 });
 
-//run application on a port
-app.listen(8080);
-console.log("Application is listening on port no 8080")
+// Run app
+app.listen(8080, function () {
+    console.log("App is running on http://localhost:8080");
+});
